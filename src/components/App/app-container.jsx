4cbef3ca263b0ad6a274b19045fd2e48c8d67908
Troy Moreland <tmoreland@idauto.net>
2020-02-03 10:24:02 -0600

    updated login page and process

--- src/components/App/app-container.jsx
+++ src/components/App/app-container.jsx
@@ -1,39 +1,63 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { withRouter } from 'react-router-dom'
 import { useCookies } from 'react-cookie'
-import { MuiThemeProvider, createMuiTheme } from '@material-ui/core'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
 
+import { firebaseAuth } from '../../services/firebase'
+import AppPage from './app-page'
 import Public from '../Public'
 import Private from '../Private'
 
+import constants from '../../constants'
+
+library.add(fab)
+
 const App = ({ history }) => {
-   const [cookies] = useCookies(['session', 'drugs'])
+   const [cookies, setCookie, removeCookie] = useCookies(['session', 'drugs'])
+   const authenticating = cookies.authenticating ? true : false
    const authenticated = cookies.session ? true : false
-   const theme = createMuiTheme({
-      palette: {
-         type: 'light',
-         primary: {
-            main: '#418FDE',
-         },
-         secondary: {
-            main: '#C8102E',
-         },
-      },
-   })
+   const { routes } = constants
+
    const propsPublic = { history }
    const propsPrivate = { history }
 
-   return (
-      <React.Fragment>
-         <MuiThemeProvider theme={theme}>
-            {authenticated ? (
-               <Private {...propsPrivate} />
-            ) : (
-               <Public {...propsPublic} />
-            )}
-         </MuiThemeProvider>
-      </React.Fragment>
-   )
+   useEffect(() => {
+      firebaseAuth().onAuthStateChanged(session => {
+         console.log(
+            'login-container',
+            'onAuthStateChanged',
+            'session',
+            session
+         )
+         if (session) {
+            initSession(session)
+         }
+      })
+
+      if (authenticated) {
+         setCookie('authenticating', true, { path: '/' })
+      }
+   }, [authenticated])
+
+   useEffect(() => {
+      console.log('App', 'authenticating', authenticating)
+   }, [authenticating])
+
+   const initSession = session => {
+      setCookie('session', session, { path: routes.root })
+      removeCookie('authenticating', { path: '/' })
+   }
+
+   if (cookies.authenticating) {
+      return <AppPage open={authenticating} />
+   }
+
+   if (authenticated) {
+      return <Private {...propsPrivate} />
+   }
+
+   return <Public {...propsPublic} />
 }
 
 export default withRouter(App)
--- src/components/Public/Login/login-page.jsx
+++ src/components/Public/Login/login-page.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
+import Box from '@material-ui/core/Box'
 import Button from '@material-ui/core/Button'
-import Grid from '@material-ui/core/Grid'
+import Icon from '@material-ui/core/Icon'
+import Typography from '@material-ui/core/Typography'
+import { makeStyles, useTheme } from '@material-ui/core/styles'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import LoginTemplate from './login-template'
 
 const LoginPage = ({ providers }) => {
    const {
@@ -9,45 +15,105 @@ const LoginPage = ({ providers }) => {
       handleLoginWithGoogle,
    } = providers
 
-   return (
-      <React.Fragment>
-         <Grid
-            container
-            direction="column"
-            alignItems="center"
-            alignContent="center"
-            spacing={4}
-         >
-            <Grid item>
-               <Button
-                  color="default"
-                  variant="contained"
-                  onClick={handleLoginWithFacebook}
-               >
-                  Login with Facebook
-               </Button>
-            </Grid>
-            <Grid item>
-               <Button
-                  color="primary"
-                  variant="contained"
-                  onClick={handleLoginWithGithub}
-               >
-                  Login with Github
-               </Button>
-            </Grid>
-            <Grid item>
-               <Button
-                  color="secondary"
-                  variant="contained"
-                  onClick={handleLoginWithGoogle}
-               >
-                  Login with Google
-               </Button>
-            </Grid>
-         </Grid>
-      </React.Fragment>
+   const theme = useTheme()
+   const useStyles = makeStyles({
+      appBarBrand: {
+         color: theme.palette.primary.main,
+         fontWeight: '700',
+         marginLeft: 'auto',
+         marginRight: 'auto',
+      },
+      appBarBrandSecondary: {
+         color: theme.palette.secondary.main,
+         fontWeight: '700',
+      },
+      appBarBrandLite: {
+         fontStyle: 'italic',
+         fontWeight: '200',
+      },
+      facebookButton: {
+         color: theme.palette.primary.contrastText,
+         width: '250px',
+         backgroundColor: '#3b5999',
+      },
+      githubButton: {
+         color: theme.palette.primary.contrastText,
+         width: '250px',
+         backgroundColor: '#24292E',
+      },
+      googleButton: {
+         color: theme.palette.primary.contrastText,
+         width: '250px',
+         backgroundColor: '#ea4335',
+      },
+      footer: {
+         color: '#24292E',
+      },
+   })
+   const classes = useStyles()
+
+   const title = (
+      <Typography className={classes.appBarBrand} variant="h5">
+         <span>
+            Keeper
+            <span className={classes.appBarBrandSecondary}>Rx</span>{' '}
+            <span className={classes.appBarBrandLite}>Lite</span>
+         </span>
+      </Typography>
    )
+
+   const FacebookButton = (
+      <Button
+         className={classes.facebookButton}
+         size="large"
+         startIcon={<FontAwesomeIcon icon={['fab', 'facebook']} />}
+         variant="contained"
+         onClick={handleLoginWithFacebook}
+      >
+         Login with Facebook
+      </Button>
+   )
+
+   const GithubButton = (
+      <Button
+         className={classes.githubButton}
+         size="large"
+         startIcon={<FontAwesomeIcon icon={['fab', 'github']} />}
+         variant="contained"
+         onClick={handleLoginWithGithub}
+      >
+         Login with Github
+      </Button>
+   )
+
+   const GoogleButton = (
+      <Button
+         className={classes.googleButton}
+         size="large"
+         startIcon={<FontAwesomeIcon icon={['fab', 'google']} />}
+         variant="contained"
+         onClick={handleLoginWithGoogle}
+      >
+         Login with Google
+      </Button>
+   )
+
+   const footer = (
+      <Typography className={classes.footer} variant="caption">
+         34 Fame, LLC - KeeperRx Lite {process.env.REACT_APP_VERSION}
+      </Typography>
+   )
+   const propsLoginTemplate = {
+      state: {
+         footer,
+         title,
+         FacebookButton,
+         GithubButton,
+         GoogleButton,
+      },
+   }
+
+   return <LoginTemplate {...propsLoginTemplate} />
 }
 
 export default LoginPage
