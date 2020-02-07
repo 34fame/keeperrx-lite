cde06a2b52a299c573af1371b20d8d777296793a
Troy Moreland <tmoreland@idauto.net>
2020-02-06 23:05:12 -0600

    modified login flow to use getRedirectResult instead of onAuthStateChange

--- src/services/firebase/firebase.js
+++ src/services/firebase/firebase.js
@@ -11,8 +11,12 @@ firebase.initializeApp({
 })
 
 const facebookProvider = new firebase.auth.FacebookAuthProvider()
+
 const githubProvider = new firebase.auth.GithubAuthProvider()
+githubProvider.addScope('repo')
+
 const googleProvider = new firebase.auth.GoogleAuthProvider()
+
 const firebaseAuth = firebase.auth
 
 export { firebaseAuth, facebookProvider, githubProvider, googleProvider }
