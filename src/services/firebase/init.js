975e82f6ee44e68964fe370d5664226fb3fa3cf9
Troy Moreland <tmoreland@idauto.net>
2020-02-21 14:31:51 -0600

    troubleshooting analytics

--- src/services/firebase/init.js
+++ src/services/firebase/init.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app'
 import 'firebase/auth'
 import 'firebase/firestore'
+import 'firebase/analytics'
 
 firebase.initializeApp({
    apiKey: process.env.REACT_APP_FIREBASE_KEY,
@@ -9,6 +10,8 @@ firebase.initializeApp({
    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+   appId: process.env.REACT_APP_FIREBASE_APP_ID,
+   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 })
 
 export default firebase
